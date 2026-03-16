a02ce22681a2b09473da993ccc193f06f6346b4c
Benjamin_The_Terrible <benjaminrutkowski@web.de>
2026-03-16 10:24:12 +0100

    Update docker_install_v1.sh

--- docker_install_v1.sh
+++ docker_install_v1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash                                                                                                                                       
 # Shabang mit dem Verweis auf die Bash-Shell
+# Wir beginnen, indem wir alle alten Dockerfiles und Docker selbst löschen
 
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1) -y        # Entfernt alle altlasten von Docker, sofernvorhanden
 
@@ -11,31 +12,30 @@ sudo rm -rf /var/lib/containerd                                                 
 sudo rm /etc/apt/sources.list.d/docker.sources                                                                                                   # Quelle löschen                                                                                                 
 sudo rm /etc/apt/keyrings/docker.asc                                                                                                             # Docker Schlüssel löschen
 
-# Alles neu
+# Jetzt installieren wir Docker von Grund auf neu
 
-# Add Docker's official GPG key:
+# Füge Docker seine offiziellen GPG Schlüssel hinzu:
 sudo apt update                                                                                                                                  # Update
 sudo apt install ca-certificates curl                                                                                                            # vertrauenswürdiges Root-Zertifikat
 sudo install -m 0755 -d /etc/apt/keyrings                                                                                                        # Neue Diractory erstelle, falls noch nicht vorhanden und Rechte vergeben
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc                                                     # GPG-Schlüssek runterladen und bei docker.asc speichern           
 sudo chmod a+r /etc/apt/keyrings/docker.asc                                                                                                      # Leserecht für alle User für docker.asc hinzufügen
 
-# Add the repository to Apt sources:
-sudo tee /etc/apt/sources.list.d/docker.sources <<EOF                                                                                            # Bis EOF wir folgendes in die Datei docker.sources geschrieben
-Types: deb                                                                                                                                       # Typ: Debian 
-URIs: https://download.docker.com/linux/ubuntu                                                                                                   # Downloadpfad
-Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")                                                                     # Ubuntu-Verion wird automatisch erkannt
-Components: stable                                                                                                                               # Stabiles Datenpaket
-Signed-By: /etc/apt/keyrings/docker.asc                                                                                                          # Lässt nur Dateien zu, die mit GPG-Schlüssel signiert sind
-EOF                                                                                                                                              # End of File (EOF)
+# #Das Repository dem Apt Quellenverzeichnis hinzufügen: Bis EOF wir folgendes in die Datei docker.sources geschrieben, Typ: Debian,Downloadpfad, Ubuntu-Verion wird automatisch erkannt, Stabiles Datenpaket, Lässt nur Dateien zu, die mit GPG-Schlüssel signiert sind, End of File (EOF)
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF                                                                                           
+Types: deb                                                                                                                                       
+URIs: https://download.docker.com/linux/ubuntu                                                                                                   
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")                                                                    
+Components: stable                                                                                                                              
+Signed-By: /etc/apt/keyrings/docker.asc                                                                                                          
+EOF                                                                                                                                             
 
-sudo apt update                                                                                                                                  # Update
+sudo apt update                                                                                                                                  
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y                                             # Docker + Docker-Compose instalieren
-
-
-sudo systemctl enable docker.service                                                                                                             # Docker Dienst starten
-sudo systemctl enable containerd.service                                                                                                         # Container Dienst starten
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y                                             
 
 
-print("Das hat geklapp")                                                                                                                          # Ausgabe um zu überprüfen, ob installation durchgelaufen ist
+sudo systemctl enable docker.service                                                                                                            
+sudo systemctl enable containerd.service                                                                                                         
+
+                                                                                                                  
